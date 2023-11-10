484521aac51aae0ef32874ee79efa8238ced7216
504-125 <504-125@DESKTOP-H2H2KSG>
2023-11-10 09:37:04 +0900

    서블릿 수정

--- b_beam/src/main/java/app/controller/PointController.java
+++ b_beam/src/main/java/app/controller/PointController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ContentsController
  */
-@WebServlet("/BoardController")
+@WebServlet("/PointController")
 public class PointController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
