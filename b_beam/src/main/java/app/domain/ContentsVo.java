557c775fce105062205c1ee233c2e691e9066539
504-125 <504-125@DESKTOP-H2H2KSG>
2023-11-16 10:21:24 +0900

    pm 생성

--- b_beam/src/main/java/app/domain/ContentsVo.java
+++ b_beam/src/main/java/app/domain/ContentsVo.java
@@ -7,9 +7,65 @@ public class ContentsVo {
 	private String mapx;
 	private String mapy;
 	private String firstimage;
+	private String firstimage2;
+	private String cat1;
+	private String cat2;
+	private String cat3;
 	private String contentdate;
 	private String contentdatem;
 	private String contentDelyn;
+	public String getFirstimage2() {
+		return firstimage2;
+	}
+	public void setFirstimage2(String firstimage2) {
+		this.firstimage2 = firstimage2;
+	}
+	public String getCat1() {
+		return cat1;
+	}
+	public void setCat1(String cat1) {
+		this.cat1 = cat1;
+	}
+	public String getCat2() {
+		return cat2;
+	}
+	public void setCat2(String cat2) {
+		this.cat2 = cat2;
+	}
+	public String getCat3() {
+		return cat3;
+	}
+	public void setCat3(String cat3) {
+		this.cat3 = cat3;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	private String tel;
+	private String zipcode;
+	private String addr1;
+	private String addr2;
 	private int bookMarkCnt;
 	
 	public String getContentid() {
---
+++ b_beam/src/main/java/app/domain/SearchCriteria.java
@@ -0,0 +1,16 @@
+package app.domain;
+
+public class SearchCriteria extends Criteria{
+	private String contentsTypeId;
+	private String SearchTypeId;
+	private String keyword;
+
+	public String getContentsTypeId()	{	return contentsTypeId;	}
+	public String getSearchTypeId()		{	return SearchTypeId;	}
+	public String getKeyword()			{	return keyword;			}
+
+	public void setContentsTypeId(String contentsTypeId)	{	this.SearchTypeId = contentsTypeId;	}
+	public void setSearchTypeId(String SearchTypeId)		{	this.SearchTypeId = SearchTypeId;	}
+	public void setKeyword(String keyword)					{	this.keyword = keyword;				}
+
+}
