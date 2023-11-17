af866c086d5cec9cffc16e24d1433102fb6d1098
kmh9600 <kingsroad96@naver.com>
2023-11-17 14:29:00 +0900

    asd

--- b_beam/src/main/java/app/domain/SearchCriteria.java
+++ b_beam/src/main/java/app/domain/SearchCriteria.java
@@ -2,15 +2,15 @@ package app.domain;
 
 public class SearchCriteria extends Criteria{
 	private String contentsTypeId;
-	private String SearchTypeId;
+	private String SearchType;
 	private String keyword;
 
 	public String getContentsTypeId()	{	return contentsTypeId;	}
-	public String getSearchTypeId()		{	return SearchTypeId;	}
+	public String getSearchType()		{	return SearchType;	}
 	public String getKeyword()			{	return keyword;			}
 
 	public void setContentsTypeId(String contentsTypeId)	{	this.contentsTypeId = contentsTypeId;	}
-	public void setSearchTypeId(String SearchTypeId)		{	this.SearchTypeId = SearchTypeId;	}
-	public void setKeyword(String keyword)					{	this.keyword = keyword;				}
+	public void setSearchType(String SearchType)			{	this.SearchType = SearchType;			}
+	public void setKeyword(String keyword)					{	this.keyword = keyword;					}
 
 }
