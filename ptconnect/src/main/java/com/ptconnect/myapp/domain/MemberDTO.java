f87cf30ea1beb1c8a1621124c3edd61abcb9f1d1
504-125 <504-125@DESKTOP-H2H2KSG>
2024-01-08 14:29:11 +0900

    멤버 DTO 추가(상속해서 사용하세요)

--- ptconnect/src/main/java/com/ptconnect/myapp/domain/MemberDTO.java
+++ ptconnect/src/main/java/com/ptconnect/myapp/domain/MemberDTO.java
@@ -1,5 +1,17 @@
 package com.ptconnect.myapp.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MemberDTO {
-	
+	private int mbNo;
+	private String mbAuth;
+	private String mbEmail;
+	private String mbPwd;
+	private String mbName;
+	private int mbPhone;
+	private String mbAddr;
+	private String mbDate;
 }
